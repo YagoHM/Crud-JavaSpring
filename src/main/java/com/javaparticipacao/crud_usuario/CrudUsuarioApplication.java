1d965a7134bb774c1c959baa01dce1ed867cf014
YagoHM <yagohamazaki@gmail.com>
2025-10-13 14:08:35 -0300

    first commit

---
+++ src/main/java/com/javaparticipacao/crud_usuario/CrudUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.javaparticipacao.crud_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsuarioApplication.class, args);
+	}
+
+}
